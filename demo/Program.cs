ddd1c8c20d2f6807a2e36704735a8051acb8412a
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-18 20:06:14 +0200

    01 Prepere for CRUD operations

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace demo
+﻿using System;
+
+namespace demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Prepere for CRUD operations
+            // CRUD :: Create - Read - Update - Delete
+            // AppDbContext.context = new AppDbContext();
+            //try
+            //{
+            //   // code
+            //}
+            //finally
+            //{
+            //  // context.Dispose()
+            //}
+
+            //using (AppDbContext.context = new AppDbContext())    // Syntax Sugar
+            //{
+            //   // CRUD code
+            //}
+
+            // using ppDbContext.context = new AppDbContext();    // Syntax Sugar
+            #endregion
         }
     }
 }
